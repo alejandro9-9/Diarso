d9386968262855f2b321896d02cf4f5a4a67a06a
Santiago Loaiza <santiago.loaiza.vasquez@gmail.com>
2026-06-26 20:22:10 -0500

    Emergencia_Contacto(revision)

---
+++ PORTAL_ACADEMICO_BACKEND/PORTAL_ACADEMICO/Emergencia_Contacto/Emergencia_ContactoCreatedEvent.cs
@@ -0,0 +1,8 @@
+﻿using PORTAL_ACADEMICO_DOMAIN.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PORTAL_ACADEMICO_DOMAIN.Emergencia_Contacto;
+
+public sealed record Emergencia_ContactoCreatedEvent(Guid Id): IDomainEvent;
---
+++ PORTAL_ACADEMICO_BACKEND/PORTAL_ACADEMICO/Emergencia_Contacto/IEmergencia_ContactoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PORTAL_ACADEMICO_DOMAIN.Emergencia_Contacto;
+
+public interface IEmergencia_ContactoRepository
+{ 
+
+    Task<Emergencia_Contacto> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<List<Emergencia_Contacto>> GetAllAsync(CancellationToken cancellationToken = default);
+    void Add(Emergencia_Contacto emergencia_contacto);
+    void Update(Guid id);
+    void Delete(Emergencia_Contacto emergencia_contacto);
+
+}
